06c38a3effb7d9f297e7d59487fb3866d24b2bce
khadiza21 <bibikhadiza474@gmail.com>
2022-04-17 02:39:41 +0600

    firebase auth react rout assignment 10

---
+++ src/firebase.init.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDavReSdJDJj8sb-QHNvM3Yhw0PCJ0f4_k",
+  authDomain: "tralive-bk-auth.firebaseapp.com",
+  projectId: "tralive-bk-auth",
+  storageBucket: "tralive-bk-auth.appspot.com",
+  messagingSenderId: "570303581700",
+  appId: "1:570303581700:web:dbc68ab00188ab7bc36f51"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+export default auth;
+
+/* 
+step 3: install firebase cli : npm install -g firebase-tools
+setp 4: 
+1. firebase login
+2. firebase init
+3. firebase deploy
+*/
