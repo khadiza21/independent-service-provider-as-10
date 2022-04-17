a5a4826cced19ecc7b251f4dfa317480831220a6
khadiza21 <bibikhadiza474@gmail.com>
2022-04-17 16:07:24 +0600

    login  setup

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    console.log(email, password);
+  };
+
+  const navigateRegister = (event) => {
+    navigate("/register");
+  };
+
   return (
-    <div className="container w-50 mx-auto">
+    <div className="container w-50 mx-auto my-5 py-5">
       <h3 className="text-primary text-center mt-2">Please LoiIn</h3>
-      <Form>
+      <Form onSubmit={handleSubmit} className="my-5 py-5">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -16,7 +39,12 @@ const Login = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
@@ -24,6 +52,12 @@ const Login = () => {
         <Button variant="primary" type="submit">
           Submit
         </Button>
+        <p>
+          New to Tralve BK?{" "}
+          <small onClick={navigateRegister} className="text-danger">
+            Please Register
+          </small>{" "}
+        </p>
       </Form>
     </div>
   );
