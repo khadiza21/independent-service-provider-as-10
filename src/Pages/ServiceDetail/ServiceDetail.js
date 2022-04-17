25d3833566eaf402fde83cb650dadcb98ea6c8d1
khadiza21 <bibikhadiza474@gmail.com>
2022-04-17 20:31:21 +0600

    email and password register and  login setup

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const {serviceId} = useParams();
     return (
         <div>
             <h3>welcome to see details {serviceId}</h3>
+            <div className="text-center">
+                  <Link to="/checkout">
+                      <button className='btn btn-primary'>Proceed Checkout</button>
+                  </Link>
+            </div>
+          
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,15 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const Header = () => {
+  const [user] = useAuthState(auth);
+  const handleSignOut = () =>{
+    signOut(auth);
+  }
   return (
     <Navbar collapseOnSelect expand="lg" sticky="top" bg="dark" variant="dark">
       <Container>
@@ -19,9 +26,13 @@ const Header = () => {
             <Nav.Link as={Link} to="about">
               About
             </Nav.Link>
-            <Nav.Link as={Link} to="login">
-              Login
-            </Nav.Link>
+            {user ? (
+              <button className='btn btn-warning text-dark btn-link text-decoration-none' onClick={handleSignOut}>Sign Out</button>
+            ) : (
+              <Nav.Link as={Link} to="login">
+                Login
+              </Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
